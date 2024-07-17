4d3797bcafc51f52cb721b8493ea15f9cbc48259
wojciechdrabczyk <wojciechdrabczyk@icloud.com>
2024-07-17 13:44:57 +0200

    feat:buttons

--- resources/js/Pages/Post/Show.jsx
+++ resources/js/Pages/Post/Show.jsx
@@ -7,6 +7,18 @@ export default function Show({post}) {
             <div>
                 {post.body}
             </div>
+            <div className={"flex flex-row gap-2"}>
+                <form action="">
+                    <button className={"bg-red-400 rounded-md text-sm px-4 py-1 text-white"}>
+                        Delete
+                    </button>
+                </form>
+                <form action="">
+                    <button className={"bg-blue-400 rounded-md text-sm px-4 py-1 text-white"}>
+                        Edit
+                    </button>
+                </form>
+            </div>
         </div>
     )
 }
